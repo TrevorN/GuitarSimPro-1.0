1fc73752839305df14141f5d40b8b68803b8d653
Athaddius <a_shmidty@hotmail.com>
2012-12-06 10:45:31 -0800

    added guitar string

--- GuitarString.java
+++ GuitarString.java
@@ -2,7 +2,9 @@ import java.util.Random;
 public class GuitarString{
 	RingQueue myQueue;
 	Random myRandom = new Random();
+	int time = 0;
 	int length;
+	final float decay = .994;
 	GuitarString(double freq){
 		length = freq/44100;
 		myQueue = new RingQueue(length);
@@ -22,6 +24,26 @@ public class GuitarString{
 		}
 
 	}
+
+	void tic(){
+
+		myQueue.enqueue(decay*(myQueue.dequque()+myQueue.peek())/2);
+		time++;
+
+	}
+
+	double sample(){
+
+		return myQueue.peek();
+
+	}
+
+	int time(){
+
+		return time;
+
+	}
+
 
 }
 

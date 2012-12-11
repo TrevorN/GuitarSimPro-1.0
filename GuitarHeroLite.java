4f62f0a74db1c34ea98fba22fccf22ade8c4c2cb
Trevor Nielsen <trevor.kn@gmail.com>
2012-12-10 18:01:31 -0800

    finished it

--- GuitarHeroLite.java
+++ GuitarHeroLite.java
@@ -3,33 +3,43 @@ class GuitarHeroLite {
 	public static void main(String[] args) {
 
 		// create two guitar strings, for concert A and C
-		double CONCERT_A = 440.0;
-		double CONCERT_C = CONCERT_A * Math.pow(1.05956, 3.0);
 		GuitarString[] strings = new GuitarString[37];
 		String keys = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
 		for(int i = 0; i < 37; i++)
 		{
-			double thing = (880 - 110)/37;
+			double thing = (880.0 - 110.0) / 36.0;
+			strings[i] = new GuitarString((int) 110 + (thing * i));
+		}
 
 		while (true) {
 
 			// check if the user has typed a key; if so, process it   
 			if (StdDraw.hasNextKeyTyped()) {
 				char key = StdDraw.nextKeyTyped();
-				if      (key == 'a') { stringA.pluck(); }
-				else if (key == 'c') { stringC.pluck(); }
+				int index = keys.indexOf(key);
+
+				if(index != -1)
+				{
+					strings[index].pluck();
+				}
 			}
 
 			// compute the superposition of samples
-			double sample = stringA.sample() + stringC.sample();
+			double sample = 0;
+			for(int i = 0; i < 37; i++)
+			{
+				sample += strings[i].sample();
+			}
 
 			// play the sample on standard audio
 			StdAudio.play(sample);
 
-			// advance the simulation of each guitar string by one step   
-			stringA.tic();
-			stringC.tic();
+			// advance the simulation of each guitar string by one step.
+			for(int i = 0; i < 37; i++)
+			{
+				strings[i].tic();
+			}
 		}
 	}
 }
